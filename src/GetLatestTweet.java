63a402cead21c749bb338efbb6c0393726ed537f
Cenethua <Selenaminez@gmail.com>
2017-12-30 14:29:59 -0800

    GetLatestTweet
    Worked on it at home for a few minutes.

--- src/GetLatestTweet.java
+++ src/GetLatestTweet.java
@@ -1,9 +1,11 @@
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
 
-public class GetLatestTweet {
+public class GetLatestTweet implements ActionListener {
 	JFrame frame;
 	JPanel panel;
 	JButton button;
@@ -14,9 +16,32 @@ public class GetLatestTweet {
 }
 
 private void newUI() {
+    // frame is visible 
 	frame = new JFrame();
-	frame.setVisible(true);
-	frame.add(panel);
+        frame.setSize(500,400);
+        frame.setVisible(true);
+        
+      //button and text added to panel 
+        panel = new JPanel();
+        button = new JButton();
+        text= new JTextField(20);
+        panel.add(button);
+        panel.add(text);
+        
+        button.addActionListener(this);
+     
+        //panel added to frame 
+        frame.add(panel);
+        
+       //frame pack and exit 
+        frame.pack();
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+                
 	
 }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        
+    }
 }
