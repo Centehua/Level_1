ddeeaa3d6d72a090ee0f4321120b685124803ab0
Centehua <league@iMac.attlocal.net>
2018-02-18 11:28:26 -0800

    CowTimer and NetflixClasses
    Finished CowTimer
    Started Movie, NetflixQueue, and NetflixRunner

---
+++ src/NetflixRunner.java
@@ -0,0 +1,12 @@
+
+public class NetflixRunner {
+	public static void main(String[] args) {
+	Movie m1 = new Movie("Black Panther",10);
+	Movie m2 = new Movie("Spider-Man: Homecoming",8.2);
+	Movie m3 = new Movie("Captain America: Civil War",8);
+	Movie m4 = new Movie("Justice League",5.2);
+	Movie m5 = new Movie("Wonder Woman",8.1);
+	
+	
+	}
+}
