df6fdba2156cdaf94c732fd947c2a117d4a914f6
grace04 <league@iMac.attlocal.net>
2018-03-04 11:27:03 -0800

    MethodsWorksheet
    Need to finish Animal method

---
+++ src/Animal.java
@@ -0,0 +1,35 @@
+
+public class Animal {
+	
+	public static void main(String[] args) {
+		Animal a1 = new Animal();
+		
+		boolean b1 =a1.catEat("icecream");
+		String s1 = 
+		
+		if(b1==false) {
+			System.out.println("Cats cannot eat" +);
+		}else{
+			System.out.println("Cats can eat"+ );
+		} 
+		
+		
+		
+	}
+	
+boolean catEat(String food) {
+	if(food.equals("bananas")) {
+		
+		return true;
+		
+	} else if (food.equals("cat food")) {
+		return true;
+	} else {
+		return false; 
+	} 
+	
+} 
+
+
+
+}
---
+++ src/MethodsWorksheet.java
@@ -0,0 +1,25 @@
+import javax.swing.JOptionPane;
+
+public class MethodsWorksheet {
+	
+	public static void main(String[] args) {
+	
+	}
+	//1 
+	String whichPresent(String friendName) {
+		
+		if(friendName.equalsIgnoreCase("Brooke")) {
+			String whichPresent = "Manga";
+			return whichPresent;
+		} else {
+			String whichPresent = "Nothing";
+			return whichPresent;
+			
+		} 
+		
+	} 
+	
+	//2 
+	
+	
+}
